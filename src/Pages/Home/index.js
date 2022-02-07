39cbc3a4747151d14e0632c6ed43d171c40af157
GustBitencourt <gubiten@gmail.com>
2022-02-07 15:28:37 -0300

    componetizando grid com camisas  para componente card  em home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -7,6 +7,7 @@ import {
   makeStyles,
 } from "@material-ui/core/";
 import Item from "../../components/Item";
+import Card from "../../components/Card";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,79 +29,27 @@ const Home = () => {
         <Paper className={classes.paper}>
           <Typography variant="h5">Categorias</Typography>
           <List>
-            <Item name="Clubes Nacionais" quantidade="5"/>
+            <Item name="Clubes Nacionais" quantidade="5" />
 
-            <Item name="Clubes Internacionais" quantidade="3"/>
-            
-            <Item name="Camisas Retrôs" quantidade="4"/>
+            <Item name="Clubes Internacionais" quantidade="3" />
+
+            <Item name="Camisas Retrôs" quantidade="4" />
           </List>
         </Paper>
       </Grid>
       <Grid container xs={9} spacing={3} className={classes.root}>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-            <Grid container direction="column">
-              <Grid item>
-                <img
-                  width="140px"
-                  src="./images/produtos/fla.jpg"
-                  alt="Flamengo 2020"
-                />
-                <Typography variant="h6">Produto</Typography>
-                <Typography variant="subtitle1">R$ 99.00</Typography>
-              </Grid>
-              <Button variant="contained">Adicionar</Button>
-            </Grid>
-          </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-            <Grid container direction="column">
-              <Grid item>
-                <img
-                  width="140px"
-                  src="./images/produtos/fla.jpg"
-                  alt="Flamengo 2020"
-                />
-                <Typography variant="h6">Produto</Typography>
-                <Typography variant="subtitle1">R$ 99.00</Typography>
-              </Grid>
-              <Button variant="contained">Adicionar</Button>
-            </Grid>
-          </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-            <Grid container direction="column">
-              <Grid item>
-                <img
-                  width="140px"
-                  src="./images/produtos/fla.jpg"
-                  alt="Flamengo 2020"
-                />
-                <Typography variant="h6">Produto</Typography>
-                <Typography variant="subtitle1">R$ 99.00</Typography>
-              </Grid>
-              <Button variant="contained">Adicionar</Button>
-            </Grid>
-          </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-            <Grid container direction="column">
-              <Grid item>
-                <img
-                  width="140px"
-                  src="./images/produtos/fla.jpg"
-                  alt="Flamengo 2020"
-                />
-                <Typography variant="h6">Produto</Typography>
-                <Typography variant="subtitle1">R$ 99.00</Typography>
-              </Grid>
-              <Button variant="contained">Adicionar</Button>
-            </Grid>
-          </Paper>
-        </Grid>
+        <Card name="Flamengo" price="129.90" urlImg="./images/produtos/fla.jpg">
+            Flamengo
+        </Card>
+        <Card name="Milan" price="129.90" urlImg="./images/produtos/milan.jpg">
+            Milan
+        </Card>
+        <Card name="Brasil" price="129.90" urlImg="./images/produtos/brasil.jpg">
+            Brasil
+        </Card>
+        <Card name="Arsenal" price="129.90" urlImg="./images/produtos/arsenal.jpg">
+            Arsenal
+        </Card>
       </Grid>
     </Grid>
   );
