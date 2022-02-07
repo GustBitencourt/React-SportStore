6a9d14433636e855907c62a48e5d0a60cccc1e10
GustBitencourt <gubiten@gmail.com>
2022-02-07 14:36:24 -0300

    transferindo container do materialUI  de home para o app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MainRoutes from './routes/routes';
 
+import { Container } from '@material-ui/core/';
+
 const App = () => {
 
 
   return(
-    <BrowserRouter>
-      <Header />
-      <MainRoutes />
-    </BrowserRouter>
+    <Container maxWidth="xl">
+      <BrowserRouter>
+        <Header />
+        <MainRoutes />
+      </BrowserRouter>
+    </Container>
   )
 }
 
