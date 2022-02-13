f194e8a4dbc86e5e9607fc2c376c35543da0a1bb
GustBitencourt <gubiten@gmail.com>
2022-02-13 16:28:39 -0300

    post de email e menssagem  para o backend

--- src/Pages/Contato/index.js
+++ src/Pages/Contato/index.js
@@ -4,12 +4,35 @@ import { Grid, Button, TextField } from "@material-ui/core/";
 const Contato = () => {
   const url = "http://localhost:5000/message";
   const [messages, setMessages] = useState([]);
+  const [autor, setAutor] = useState("");
+  const [conteudo, setConteudo] = useState("");
+  const [validate, setValidate] = useState(false);
 
   useEffect(async () => {
-    const response = await fetch(url)
+    const response = await fetch(url);
     const data = await response.json();
     setMessages(data);
-}, [])
+  }, []);
+
+  const sendMessage = () => {
+    setValidate(false);
+    if (autor.length <= 0 || conteudo.length) {
+      return setValidate(!validate);
+    }
+
+    const bodyForm = {
+      email: autor,
+      message: conteudo
+    }
+    /* Parametros dos post */
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(bodyForm)
+    })
+  };
 
   /* useEffect(() => {
     async function loadApiInfo() {
@@ -19,16 +42,51 @@ const Contato = () => {
     }
     loadApiInfo();
   }, []); */
-  
+
   return (
     <>
       <Grid container direction="row">
-        <TextField id="name" label="Name" fullWidth />
-        <TextField id="message" label="Message" fullWidth />
+        <TextField
+          id="name"
+          label="Name"
+          value={autor}
+          onChange={(event) => setAutor(event.target.value)}
+          fullWidth
+        />
+        <TextField
+          id="message"
+          label="Message"
+          value={conteudo}
+          onChange={(event) => setConteudo(event.target.value)}
+          fullWidth
+        />
+
+        {/* Spam para erro em tentativa de enviar mensagem com campo vazio */}
+        {validate && (
+          <div
+            className="alert alert-warning alert-dismissible fade show mt-2"
+            role="alert"
+          >
+            <strong>Por favor preencha todos os campos!</strong>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+          </div>
+        )}
       </Grid>
-      <Button className="mt-2" variant="contained" color="primary">
+
+      <Button
+        onClick={sendMessage}
+        className="mt-2"
+        variant="contained"
+        color="primary"
+      >
         Sent
       </Button>
+
       {messages.map((message) => {
         return (
           <div className="card mt-2" key={message.id}>
